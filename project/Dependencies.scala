115760f58e6bd87b168442fa6c66bbf8dc8a2f8f
baha.aboukoura <baha.aboukoura@digital.hmrc.gov.uk>
2024-06-06 12:51:39 +0100

    revert changes

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,8 +1,13 @@
-import sbt.*
+import sbt._
 
 object Dependencies {
 
-  val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "performance-test-runner" % "6.0.0" % Test
+  private val gatlingVersion = "3.5.1"
+
+  val test = Seq(
+    "com.typesafe"          % "config"                    % "1.4.3"         % Test,
+    "uk.gov.hmrc"          %% "performance-test-runner"   % "5.5.0"         % Test,
+    "io.gatling"            % "gatling-test-framework"    % gatlingVersion  % Test,
+    "io.gatling.highcharts" % "gatling-charts-highcharts" % gatlingVersion  % Test
   )
 }
